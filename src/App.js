f972a7729331ea9d91d10a4416306bad5d6ff0f8
MVBharat <manebharat85@gmail.com>
2018-12-10 13:55:54 +0530

    Add new project form fields

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects'
+import AddProject from './Components/AddProject'
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state={
-      projects: [
-        {
-          title: 'Business Website',
-          category: 'web design'
-        },
-        {
-          title: 'Social App',
-          category: 'Mobile developement'
-        },
-        {
-          title: 'Ecomerce Application',
-          category: 'web developement'
-        }
-      ]
+      projects: []
     }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        title: 'Business Website',
+        category: 'web design'
+      },
+      {
+        title: 'Social App',
+        category: 'Mobile developement'
+      },
+      {
+        title: 'Ecomerce Application',
+        category: 'web developement'
+      }
+    ]})
+  }
+
+  handleAddProject(project){
+    console.log(project);
+    let projects = this.state.projects;
+    projects.push(project)
+    this.setState({projects:projects})
   }
 
   render() {
     return (
       <div className="App">
-        My App
+        <AddProject addProject = {this.handleAddProject.bind(this)} />
+        <br/>
         <Projects projects={this.state.projects} />
       </div>
     );
