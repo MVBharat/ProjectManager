6272b67c92103019d07a6f063d3123475a44ca2f
MVBharat <manebharat85@gmail.com>
2018-12-10 12:23:43 +0530

    Added Project Itmes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'web design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile developement'
+        },
+        {
+          title: 'Ecomerce Application',
+          category: 'web developement'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        My App
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
